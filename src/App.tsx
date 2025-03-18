26534528a847e71c7a57e782716f1de2f2172c71
robbytrujillo <robbyikusuma88@gmail.com>
2025-03-18 14:03:26 +0700

    Introduction vinegere App | Create Form Submit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import FormVigenere from './components/FormVigenere'
+import FormVigenere from './components/FormVigenere';
 
 function App() {
   // const [count, setCount] = useState(0)
--- src/components/FormVigenere.tsx
+++ src/components/FormVigenere.tsx
@@ -1,6 +1,25 @@
-import { ReactElement } from 'react';
+import { ReactElement, FormEvent } from 'react';
+import ShowEye from '../assets/show-eye.svg';
+import HideEye from '../assets/hide-eye.svg';
+
 
 export default function FormVigenere(): ReactElement {
+
+    function handleSubmit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+        const messages = formData.get('messages') as string; 
+        const password = formData.get('password') as string;
+        const action = (form.activeElement as HTMLButtonElement).value;
+        
+        if (action === 'encrypt') {
+            console.log('Encrypting...');
+        } else {
+            console.log('Original message...');
+        }
+    }
+    
     return (
         <>
             <form>
@@ -23,12 +42,14 @@ export default function FormVigenere(): ReactElement {
                     <button type="submit" 
                         value="encrypt" 
                         className="w-1/2 flex items-center justify-center">
-                            Encrypt
+                            <span>Encrypt </span>
+                            <img src={HideEye} alt="hide eye" className="w-4 h-4"></img>
                     </button>
                     <button type="submit" 
                         value="original" 
                         className="w-1/2 flex items-center justify-center">
-                            Original
+                            <span>Original </span>
+                            <img src={ShowEye} alt="show eye" className="w-4 h-4"></img>
                     </button>                    
                 </div>
             </form>
