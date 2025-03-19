158fb133d90532d92345c961a461468a8ba1bb33
robbytrujillo <robbyikusuma88@gmail.com>
2025-03-19 13:04:33 +0700

    Introduction vinegere App | Check Fields Messages dan Password

--- src/components/FormVigenere.tsx
+++ src/components/FormVigenere.tsx
@@ -42,6 +42,8 @@ export default function FormVigenere(): ReactElement {
     }
 
     function error(): ReactElement {
+        if (isValid) return<></>
+        
         return (
             <div className="flex items-center p-4 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800" role="alert">
                 <span className="sr-only">Info</span>
